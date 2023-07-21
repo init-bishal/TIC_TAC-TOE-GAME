38831f38948624e3fe0eeb4db3fe5de9ced0995e
Bishal Ghosh <bbishal.g01@gmail.com>
2023-07-21 09:08:23 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './styles.scss'
 import Board from './components/Board'
+import { useState } from 'react'
 function App() {
+   let [counter,setCounter]=useState(0)
+   const onBtnClick=()=>{
+         setCounter((i)=>{
+             return i+1
+         })
+   }
   return (
     <div className='app' >
-        <Board/>
+        <button onClick={onBtnClick}>Click me please</button>
+        <div>{counter}</div>
+        {/* <Board/> */}
     </div>
   )
 }
